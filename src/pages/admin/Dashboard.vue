1870db7f53d1f18676e659cf289133d2ef5eb191
unknown <phelsajc@users.noreply.github.com>
2023-12-19 10:16:07 +0800

    update select ALL

--- src/pages/admin/Dashboard.vue
+++ src/pages/admin/Dashboard.vue
@@ -1,74 +1,76 @@
 <template>
-  <base-layout :show-tool-bar-menu="true">
-    <ion-card>
-      <ion-card-header>
-        <ion-card-title>Filter</ion-card-title>
-        <!-- <ion-card-subtitle>Card Subtitle</ion-card-subtitle> -->
-      </ion-card-header>
+  <KeepAlive>
+    <base-layout :show-tool-bar-menu="true">
+      <ion-card>
+        <ion-card-header>
+          <ion-card-title>Filter</ion-card-title>
+          <!-- <ion-card-subtitle>Card Subtitle</ion-card-subtitle> -->
+        </ion-card-header>
 
-      <ion-card-content>
-        <ion-grid size="12">
-          <ion-row>
-            <ion-col size="12">
-              Date
-              <VueDatePicker
-                v-model="form.date"
-                :format="'YYY-dd-MM'"
-                :teleport="true"
-                range
-              ></VueDatePicker
-            ></ion-col>
-          </ion-row>
-          <ion-row>
-            <ion-col size="12">
-              <ion-item>
-                <ion-select
-                  label="Stations"
-                  v-model="form.stns"
-                  placeholder="Select"
-                  @ionChange="selectStn($event)"
-                  :multiple="true"
-                >
-                  <ion-select-option v-for="e in stn_list" :value="e">{{
-                    e
-                  }}</ion-select-option>
-                </ion-select>
-              </ion-item>
-            </ion-col>
-          </ion-row>
-          <ion-row>
-            <ion-col size="12">
-              <ion-item>
-                <ion-label>Total Regular Beds</ion-label>
-                <ion-badge color="success">{{ total_reg_beds }}</ion-badge>
-              </ion-item>
-            </ion-col>
-          </ion-row>
-          <ion-row>
-            <ion-col size="12">
+        <ion-card-content>
+          <ion-grid size="12">
+            <ion-row>
+              <ion-col size="12">
+                Date
+                <VueDatePicker
+                  v-model="form.date"
+                  :format="'YYY-dd-MM'"
+                  :teleport="true"
+                  range
+                ></VueDatePicker
+              ></ion-col>
+            </ion-row>
+            <ion-row>
+              <ion-col size="12">
+                <ion-item>
+                  <ion-select
+                    label="Stations"
+                    v-model="form.stns"
+                    placeholder="Select"
+                    @ionChange="selectStn($event)"
+                    :multiple="true"
+                  >
+                    <ion-select-option v-for="e in stn_list" :value="e">{{
+                      e
+                    }}</ion-select-option>
+                  </ion-select>
+                </ion-item>
+              </ion-col>
+            </ion-row>
+            <ion-row>
+              <ion-col size="12">
+                <ion-item>
+                  <ion-label>Total Regular Beds</ion-label>
+                  <ion-badge color="success">{{ total_reg_beds }}</ion-badge>
+                </ion-item>
+              </ion-col>
+            </ion-row>
+            <ion-row>
+              <ion-col size="12">
                 <ion-button expand="block" @click="filter()">FIND</ion-button>
-            </ion-col>
-          </ion-row>
-        </ion-grid>
-      </ion-card-content>
-    </ion-card>
-    
-    <ion-card>
-      <ion-card-header>
-        <ion-card-title>Results</ion-card-title>
-      </ion-card-header>
+              </ion-col>
+            </ion-row>
+          </ion-grid>
+        </ion-card-content>
+      </ion-card>
 
-      <ion-card-content>
-        <ion-scroll scrollY="true">
-          <ion-list>
-            <ion-item v-for="e in censusResults[0]" @click="redirect(e)">
-              <ion-label>{{ e.station }}</ion-label>
-            </ion-item>
-          </ion-list>
-        </ion-scroll>
-      </ion-card-content>
-    </ion-card>
-  </base-layout>
+      <ion-card>
+        <ion-card-header>
+          <ion-card-title>Results</ion-card-title>
+        </ion-card-header>
+
+        <ion-card-content>
+          <ion-scroll scrollY="true">
+            <ion-list>
+              <ion-item v-for="e in censusResults[0]" @click="redirect(e)">
+                <ion-label>{{ e.station }}</ion-label>
+              </ion-item>
+            </ion-list>
+          </ion-scroll>
+        </ion-card-content>
+      </ion-card>
+    </base-layout>
+  </KeepAlive>
 </template>
 
 <script>
@@ -88,7 +90,7 @@ export default {
     const stnVal = ref([]);
     const stnInput = ref([]);
     const censusResults = ref([]);
-    
+
     const form = ref({
       date: null,
       fdate: null,
@@ -105,7 +107,7 @@ export default {
 
     function getResults() {
       //showLoading()
-      stn_list.value.push("ALL");
+      stn_list.value.push("All");
       Stns.list()
         .then((response) => {
           /* timeline_header.value = response.data
@@ -133,32 +135,32 @@ export default {
 
     const router = useRouter();
     const b = [
-    {
-      station: "Nursery",
-      user: 3,
-      type: "support",
-    },
-    {
-      station: "Station 11",
-      user: 3,
-      type: "support",
-    },
-    {
-      station: "Pedia Ward",
-      user: 3,
-      type: "support",
-    },
-    {
-      station: "Station 1",
-      user: 3,
-      type: "support",
-    },
-    {
-      station: "Station 3",
-      user: 3,
-      type: "support",
-    },
-  ];
+      {
+        station: "Nursery",
+        user: 3,
+        type: "support",
+      },
+      {
+        station: "Station 11",
+        user: 3,
+        type: "support",
+      },
+      {
+        station: "Pedia Ward",
+        user: 3,
+        type: "support",
+      },
+      {
+        station: "Station 1",
+        user: 3,
+        type: "support",
+      },
+      {
+        station: "Station 3",
+        user: 3,
+        type: "support",
+      },
+    ];
 
     function redirect(e) {
       router.push({
@@ -169,37 +171,51 @@ export default {
       });
     }
 
-   async function selectStn(ev) {
-    //console.log("Current value:", JSON.stringify(ev.detail.value));
-    stnVal.value = [];
-     await ev.detail.value.forEach((element) => {
-       console.log(element);
+    async function selectStn(ev) {
+      if (ev.detail.value[0] == "All") {
+        stnVal.value = [];
+        stnVal.value = ["All"];
+      } else {
+        var index = stnVal.value.indexOf("All");
+        if (index !== -1) {
+          stnVal.value.splice(index, 1);
+          stnVal.value = [];
+          await ev.detail.value.forEach((element) => {
+            console.log(element);
             stnVal.value.push(element);
-      });
+          });
+        }
+      }
+
+      /* console.log(ev.detail.value[0]);
+      stnVal.value = [];
+      await ev.detail.value.forEach((element) => {
+        console.log(element);
+        stnVal.value.push(element);
+      }); */
     }
 
     function filter() {
-      form.stnInput = stnVal.value
-     // console.log(form.value.date[0])
+      form.stnInput = stnVal.value;
+      // console.log(form.value.date[0])
       form.value.fdate = moment(form.value.date[0]).format("YYYY-MM-DD");
       form.value.tdate = moment(form.value.date[1]).format("YYYY-MM-DD");
       //console.log(form.value)
 
-      
       Census.list(form.value)
         .then((response) => {
           //  console.log(response)
-          total_reg_beds.value = response.data.totalRegularBed
-          censusResults.value.push(response.data.data)
+          total_reg_beds.value = response.data.totalRegularBed;
+          censusResults.value = [];
+          censusResults.value.push(response.data.data);
           /* response.data.data.forEach(element => {
             console.log(element)
           }); */
-           console.log(response)
-           console.log(total_reg_beds.value)
+          console.log(response);
+          console.log(total_reg_beds.value);
         })
         .catch((err) => {
           console.log(err);
-          
         })
         .finally();
     }
